de7d00dbc5f237ecd3c2984005840a328e2954da
abcd50327 <abcd50327@hotmail.com.tw>
2017-06-02 13:26:23 +0800

    20170517V2

--- LEE41.php
+++ LEE41.php
@@ -7,6 +7,7 @@ $sql = "insert into travel (tnumber,tname,tsystem,tmanager) values(?,?,?,?)";
 
 
 $json = file_get_contents('http://data.coa.gov.tw/Service/OpenData/DataFileService.aspx?UnitId=102');
+
 $root = json_decode($json);
 
 foreach ($root as $row){
--- LEE41_test.php
+++ LEE41_test.php
@@ -1,19 +1,23 @@
+<meta charset="UTF-8">
+
 <?php
 include 'sql3.php';
 $pdo = new pdo($dsn,$user,$passwd,$opt);
-$sql = "insert into tw_train(twStation_Name,twStation_EName,addr,tel)";
+$sql = "insert into tw_train(stationname,stationename,addr,tel)values(?,?,?,?)";
 
 
 $json = file_get_contents('http://www.railway.gov.tw/Upload/UserFiles/%E8%BB%8A%E7%AB%99%E5%9F%BA%E6%9C%AC%E8%B3%87%E6%96%992.json');
+$result = mb_convert_encoding($json,"utf-8","big5");
+$root = json_decode($result);
 
-$root = csv($json);
-echo var_dump($root);
 foreach ($root as $row){
-    $twStation_Name = $row->Station_Name;
-    $twStation_EName = $row->Station_EName;
+    $stationname = $row->Station_Name;
+    $stationename = $row->Station_EName;
     $addr=$row->住址;
     $tel = $row -> 電話;
 
-    $pdo->prepare($sql)->execute([$twStation_Name,$twStation_EName,$addr,$tel]);
+    $pdo->prepare($sql)->execute([$stationname,$stationename,$addr,$tel]);
 
 }
+
+//http://www.railway.gov.tw/Upload/UserFiles/%E8%BB%8A%E7%AB%99%E5%9F%BA%E6%9C%AC%E8%B3%87%E6%96%992.json
